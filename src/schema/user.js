c294c94b371ff45f3cdbc9889a21659c6cb7b2fb
Vinay14Newbie <wjust1984@gmail.com>
2024-10-27 21:54:31 +0530

    added respositories section in the project

---
+++ src/schema/user.js
@@ -0,0 +1,31 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String, 
+        required: true,
+        uique: true,
+        minLength: 5
+    },
+    email: {
+        type: String, 
+        required: true, 
+        unique: true,
+        minLength: 5,
+        validate: {
+            validator: function (emailValue) {
+                return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(emailValue);
+            }, 
+            message: "Invalid email format"
+        }
+    },
+    password: {
+        type: String, 
+        required: true, 
+        minLength: 5
+    }
+}, {timestamps: true});  //timestamps will keep updating the time of created at/ updated at.
+
+const User = mongoose.model('User', userSchema)
+
+export default User;
