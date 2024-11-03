640a15da316617042ca8fa08cf3b3e447026c698
Vinay14Newbie <wjust1984@gmail.com>
2024-11-03 08:02:13 +0530

    aws server setup succesfully

--- src/awsConfig.js
+++ src/awsConfig.js
@@ -1,11 +1,12 @@
 import AWS from 'aws-sdk';
-import { AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY } from './serverConfig.js';
+import { AWS_REGION, AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY } from './serverConfig.js';
 
 const s3 = new AWS.S3({
-    region: '',
+    region: AWS_REGION,
     accessKeyId: AWS_ACCESS_KEY,
     secretAccessKey: AWS_SECRET_ACCESS_KEY
 })
 
+console.log(AWS_REGION);
 console.log(AWS_ACCESS_KEY);
 console.log(AWS_SECRET_ACCESS_KEY);
--- src/serverConfig.js
+++ src/serverConfig.js
@@ -7,3 +7,5 @@ export const DB_URL = process.env.DB_URL;
 export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
 
 export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+
+export const AWS_REGION = process.env.AWS_REGION;
