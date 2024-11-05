282b994950ae3e32c2e3df81c00c3df4684472af
Vinay14Newbie <wjust1984@gmail.com>
2024-11-05 22:25:05 +0530

    basic api for posting the post with caption

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,6 +1,16 @@
-export const createPost = async (createPostObject) => {
-    // 1. Take the image of the post and upload on aws
-    // 2. Get the url of the image from the aws response
-    // 3. Create a post with the caption and the image url in the 'database' using repository
-    // 4. Return the post object
+// 1. Take the image of the post and upload on aws
+// 2. Get the url of the image from the aws response
+// 3. Create a post with the caption and the image url in the 'database' using repository
+// 4. Return the post object
+
+import { createPost } from "../repositories/postRepository.js";
+
+export const createPostService = async (createPostObject) => {
+    const caption = createPostObject.caption?.trim();
+    const image = createPostObject.image;
+    // const user = createPostObject.user; 
+
+    const post = await createPost(caption, image);
+
+    return post;
 }
